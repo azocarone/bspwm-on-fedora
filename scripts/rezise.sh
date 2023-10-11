0793819ca7d44adaad9467b171014c2583b7b887
Enríquez González <96206578+AlvinPix@users.noreply.github.com>
2023-10-11 19:28:03 -0400

    Update rezise.sh

--- scripts/rezise.sh
+++ scripts/rezise.sh
@@ -1,13 +1,13 @@
-# Obtén el tamaño del monitor primario (e.g., HDMI-1)
+# Get the size of the primary monitor (e.g. HDMI-1)
 monitor_size=$(xrandr | grep "primary" | awk '{print $4}')
-# Divide el tamaño en ancho y alto
+# Divide the size into width and height
 width=$(echo $monitor_size | cut -d'x' -f1)
 height=$(echo $monitor_size | cut -d'x' -f2)
-# Calcula las coordenadas para centrar la ventana en el monitor
-x_pos=$((($width - 1050) / 2))  # Ajusta el ancho deseado (800)
-y_pos=$((($height - 600) / 2))  # Ajusta el alto deseado (600)
+# Calculate the coordinates to center the window on the monitor
+x_pos=$((($width - 1050) / 2))  # Adjust the desired width (800)
+y_pos=$((($height - 600) / 2))  # Set the desired height (600)
 
-# Abre la ventana Kitty en modo flotante con las coordenadas calculadas
+# Open the Kitty window in floating mode with the calculated coordinates
 bspc node -t floating -g hidden=off
 xdotool search --classname Kitty windowsize %@ 1050 600
 xdotool search --classname Kitty windowmove %@ $x_pos $y_pos
