f92af1a2c54cd535ef6b06076c17722c7c267e02
Enríquez González <96206578+AlvinPix@users.noreply.github.com>
2023-10-13 15:03:22 -0400

    Update ethernet_status.sh

--- .themes/Ryan/scripts/ethernet_status.sh
+++ .themes/Ryan/scripts/ethernet_status.sh
@@ -2,7 +2,7 @@
 
 #echo "%{F#ffffff}  %{F#ffffff}$(/usr/sbin/ifconfig wlan0 | grep "inet " | awk '{print $2}')%{u-}"
 
-ip=$(/usr/sbin/ifconfig wlan0 | grep "inet " | awk '{print $2}')
+ip=$(/usr/sbin/ifconfig eth0 | grep "inet " | awk '{print $2}')
 
 if [ -n "$ip" ]; then
   echo "%{F#c2e6ac}  %{F#c2e6ac}$ip%{u-}"
