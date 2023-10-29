f9d46578a00c802ccade76a4004d3a429930c2e0
Enríquez González <96206578+AlvinPix@users.noreply.github.com>
2023-10-29 09:54:49 -0400

    Update script-manager.sh

--- .config/polybar/cuts/scripts/script-manager.sh
+++ .config/polybar/cuts/scripts/script-manager.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-SDIR="$HOME/.config/polybar/cuts/scripts"
-GHOSTDIR="/home/alvinpix/Escritorio/PX-games/Github/Ghost-script"
-FALCONDIR="/home/alvinpix/Escritorio/PX-games/Github/Falcon"
-RESI="/home/alvinpix/Escritorio/PX-games/Scripts"
+#SDIR="$HOME/.config/polybar/cuts/scripts"
+#GHOSTDIR="/home/alvinpix/Escritorio/PX-games/Github/Ghost-script"
+#FALCONDIR="/home/alvinpix/Escritorio/PX-games/Github/Falcon"
+#RESI="/home/alvinpix/Escritorio/PX-games/Scripts"
+USERDIR="${HOME}/scripts"
 
 # Launch Rofi
 MENU="$(rofi -no-config -no-lazy-grab -sep "|" -dmenu -i -p '' \
@@ -11,18 +12,18 @@ MENU="$(rofi -no-config -no-lazy-grab -sep "|" -dmenu -i -p '' \
 <<< " Ghost| Falcon| Updates| Wifi|")"
             case "$MENU" in
 *Ghost)
-kitty -o ~/.config/kitty/kitty.conf --hold -- bash -c "cd $RESI && ./rezise.sh && cd $GHOSTDIR && sudo ./Ghost.sh"
+kitty -o ~/.config/kitty/kitty.conf --hold -- bash -c "cd $USERDIR && ./rezise.sh && cd $GHOSTDIR && sudo ./Ghost.sh"
 ;;
 
 *Falcon)
-kitty -o ~/.config/kitty/kitty.conf --hold -- bash -c "cd $RESI && ./rezise.sh && cd $FALCONDIR && ./falcon.sh"
+kitty -o ~/.config/kitty/kitty.conf --hold -- bash -c "cd $USERDIR && ./rezise.sh && cd $FALCONDIR && ./falcon.sh"
 ;;
 
 *Updates)
-kitty -o ~/.config/kitty/kitty.conf --hold -- bash -c "cd $RESI && ./rezise.sh && cd $RESI && sudo ./updates.sh"
+kitty -o ~/.config/kitty/kitty.conf --hold -- bash -c "cd $USERDIR && ./rezise.sh && cd $USERDIR && sudo ./updates.sh"
 ;;
 
 *Wifi)
-kitty -o ~/.config/kitty/kitty.conf --hold -- bash -c "cd $RESI && ./rezise.sh && cd $RESI && ./wifi.sh"
+kitty -o ~/.config/kitty/kitty.conf --hold -- bash -c "cd $USERDIR && ./rezise.sh && cd $USERDIR && ./wifi.sh"
 ;;
         esac
