94af0bccb63990e9b1d7c95140ce222e7817337f
Enríquez González <96206578+AlvinPix@users.noreply.github.com>
2024-05-24 20:26:50 -0400

    Update script-manager.sh

--- .config/polybar/cuts/scripts/script-manager.sh
+++ .config/polybar/cuts/scripts/script-manager.sh
@@ -9,15 +9,15 @@ USERDIR="${HOME}/scripts"
 # Launch Rofi
 MENU="$(rofi -no-config -no-lazy-grab -sep "|" -dmenu -i -p '' \
 -theme $SDIR/rofi/styles.rasi \
-<<< " Ghost| Falcon| Updates| Wifi|")"
+<<< " Updates| Wifi|")"
             case "$MENU" in
-*Ghost)
-kitty -o ~/.config/kitty/kitty.conf --hold -- bash -c "cd $USERDIR && ./rezise.sh && cd $GHOSTDIR && sudo ./Ghost.sh"
-;;
+#*Ghost)
+#kitty -o ~/.config/kitty/kitty.conf --hold -- bash -c "cd $USERDIR && ./rezise.sh && cd $GHOSTDIR && sudo ./Ghost.sh"
+#;;
 
-*Falcon)
-kitty -o ~/.config/kitty/kitty.conf --hold -- bash -c "cd $USERDIR && ./rezise.sh && cd $FALCONDIR && ./falcon.sh"
-;;
+#*Falcon)
+#kitty -o ~/.config/kitty/kitty.conf --hold -- bash -c "cd $USERDIR && ./rezise.sh && cd $FALCONDIR && ./falcon.sh"
+#;;
 
 *Updates)
 kitty -o ~/.config/kitty/kitty.conf --hold -- bash -c "cd $USERDIR && ./rezise.sh && cd $USERDIR && sudo ./updates.sh"
