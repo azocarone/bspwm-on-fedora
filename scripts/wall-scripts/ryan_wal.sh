eb51366cc68d23ca287e83d4911ad8b9004268c2
Enríquez González <96206578+AlvinPix@users.noreply.github.com>
2024-05-30 23:38:24 -0400

    Update ryan_wal.sh

--- scripts/wall-scripts/ryan_wal.sh
+++ scripts/wall-scripts/ryan_wal.sh
@@ -4,7 +4,7 @@
 USERNAME=$(whoami)
 USERDIR="/home/${USERNAME}"
 
-# WALLPAPERS RAVEN
+# WALLPAPERS RYAN
 
 wallpapers_dir="/home/${USERNAME}/.themes/Ryan/wallpapers"
 wallpapers=($(find "$wallpapers_dir" -type f -name "*.png" -o -name "*.jpg" -o -name "*.jpeg"))
