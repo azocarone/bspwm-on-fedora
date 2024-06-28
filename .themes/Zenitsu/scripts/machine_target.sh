b2cf8d25c8a33d61cf61b7581779cd6cbe8d43ca
Enríquez González <96206578+AlvinPix@users.noreply.github.com>
2024-06-27 22:01:06 -0400

    Update machine_target.sh

--- .themes/Zenitsu/scripts/machine_target.sh
+++ .themes/Zenitsu/scripts/machine_target.sh
@@ -5,10 +5,10 @@ name_target=$(cat ~/.config/polybar/cuts/scripts/target | awk '{print $2}')
 
 	if [ $ip_target ] && [ $name_target ]; then
 #		echo "%{F#f1e8c5}%{F#f1e8c5} $ip_target - $name_target "
-		echo "%{A1:echo $ip_target | xclip -selection clipboard:}%{F#f1e8c5}%{F#f1e8c5} $ip_target - $name_target %{A}"
+		echo "%{A1:echo $ip_target | xclip -selection clipboard:}%{F#f1e8c5}%{F#f1e8c5} $ip_target - $name_target %{A}"
 	elif [ $(cat ~/.config/polybar/cuts/scripts/target | wc -w) -eq 1 ]; then
 #		echo "%{F#f1e8c5}%{F#f1e8c5} $ip_target "
-		echo "%{A1:echo $ip_target | xclip -selection clipboard:}%{F#f1e8c5}%{F#f1e8c5} $ip_target %{A}"
+		echo "%{A1:echo $ip_target | xclip -selection clipboard:}%{F#f1e8c5}%{F#f1e8c5} $ip_target %{A}"
 	else
-		echo "%{F#f1e8c5}%{u-}%{F#f1e8c5} No target "
+		echo "%{F#f1e8c5}%{u-}%{F#f1e8c5} No target "
 	fi
