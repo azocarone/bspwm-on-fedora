ce073ede66384da78c511dd9c753f9f7d23987a4
Enríquez González <96206578+AlvinPix@users.noreply.github.com>
2024-06-27 21:59:07 -0400

    Update machine_target.sh

--- .themes/Raven/scripts/machine_target.sh
+++ .themes/Raven/scripts/machine_target.sh
@@ -5,10 +5,10 @@ name_target=$(cat ~/.config/polybar/cuts/scripts/target | awk '{print $2}')
 
 	if [ $ip_target ] && [ $name_target ]; then
 #		echo "%{F#dbcde1}%{F#dbcde1} $ip_target - $name_target "
-		echo "%{A1:echo $ip_target | xclip -selection clipboard:}%{F#dbcde1}%{F#dbcde1} $ip_target - $name_target %{A}"
+		echo "%{A1:echo $ip_target | xclip -selection clipboard:}%{F#dbcde1}%{F#dbcde1} $ip_target - $name_target %{A}"
 	elif [ $(cat ~/.config/polybar/cuts/scripts/target | wc -w) -eq 1 ]; then
 #		echo "%{F#dbcde1}%{F#dbcde1} $ip_target "
-		echo "%{A1:echo $ip_target | xclip -selection clipboard:}%{F#dbcde1}%{F#dbcde1} $ip_target %{A}"
+		echo "%{A1:echo $ip_target | xclip -selection clipboard:}%{F#dbcde1}%{F#dbcde1} $ip_target %{A}"
 	else
-		echo "%{F#dbcde1}%{u-}%{F#dbcde1} No target "
+		echo "%{F#dbcde1}%{u-}%{F#dbcde1} No target "
 	fi
