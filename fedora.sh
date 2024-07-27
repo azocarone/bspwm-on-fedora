e63f61169221813ed43a5aa040169263147bdb89
José AZÓCAR <azocare@gmail.com>
2024-07-27 09:27:24 -0400

    Update code

--- fedora.sh
+++ fedora.sh
@@ -1,41 +1,19 @@
 #!/bin/bash
 
-define_colors() {
-    WHITE='\033[1;37m'
-    RED='\033[1;31m'
-    GREEN='\033[1;32m'
-    CYAN='\033[1;36m'
-    BLUE='\033[1;34m'
-}
-
-display_banner() {
-    local banner_file="$1"
-
-    if [ -f "$banner_file" ]; then
-        echo -e "${CYAN}" ; cat "$1"
-    else
-        echo -e "${RED}Banner file not found: $banner_file\n"
-    fi
-      
-    echo -e "${WHITE} [${BLUE}i${WHITE}] Scripts to install and configure a professional"
-    echo -e "${WHITE} [${BLUE}i${WHITE}] BSPWM environment on Fedora Linux Workstation.\n"
-    echo -e "${WHITE} [${BLUE}i${WHITE}] Hello ${RED}$(whoami)${WHITE}, installation will begin soon.\n"
-}
-
-get_list_rpm_packages() {
+get_rpm_packages() {
     local yaml_file="$1"
-    local list_rpm_packages=$(awk '
+    local rpm_packages=$(awk '
         /^[^:]+:$/ { in_list=1; next }
         /^\s*$/ { in_list=0 }
         /^\s*-\s+/ && in_list { print $2 }
     ' "$yaml_file")
     
-    echo "$list_rpm_packages"
+    echo "$rpm_packages"
 }
 
-get_list_git_packages(){
+get_git_packages(){
     local yaml_file="$1"
-    local list_git_packages=$(awk '
+    local git_packages=$(awk '
         /^\s*repo_url:/ {
             repo_url=gensub(/.*repo_url: /, "", 1)
             gsub(/"/, "", repo_url)
@@ -47,20 +25,48 @@ get_list_git_packages(){
         }
     ' "$yaml_file")
     
-    echo "$list_git_packages"
+    echo "$git_packages"
 }
 
-install_rpm_packages_from_list(){
-    local list_rpm_packages="$1"
+display_banner() {
+    local banner_file="$1"
 
-    echo -e "${WHITE} [${BLUE}i${WHITE}] Installing rpm packages:\n"
-    
-    sudo dnf upgrade -y --refresh \
-        | sudo dnf install -y ${list_rpm_packages}
+    if [ -f "$banner_file" ]; then
+        echo -e "${colors[cyan]}" ; cat "$1"
+    else
+        echo -e "${colors[red]}Banner file not found: $banner_file\n"
+    fi
+
+    echo -e "${bullets[info]} Scripts to install and configure a professional"
+    echo -e "${bullets[info]} BSPWM environment on Fedora Linux Workstation.\n"
+    echo -e "${bullets[info]} Hello ${colors[red]}$(whoami)${colors[white]}, installation will begin soon.\n"
 }
 
-deploy_git_packages_from_list(){
-    local list_git_packages="$1"
+confirm_installation() {
+    echo -ne "${bullets[question]} Do you want to continue with the installation [y/N]?: "
+    tput setaf 1
+    read -r reply
+    tput setaf 0
+
+    if [ "$reply" = "y" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+install_rpm_packages(){
+    local rpm_packages="$1"
+
+    echo -e "${bullets[info]} Linux System Update:\n"
+    sudo dnf upgrade -y --refresh
+
+    echo -e "${bullets[info]} Installing rpm packages:\n"
+    sudo dnf install -y ${rpm_packages}
+}
+
+deploy_git_packages(){
+    local git_packages="$1"
     local install_dir="$2"
 
     echo -e "${WHITE} [${BLUE}i${WHITE}] Installing packages from git.\n"
@@ -71,7 +77,7 @@ deploy_git_packages_from_list(){
             copy_executable_to_install_dir "$package_name" "$install_dir"
         fi
         cleanup_package_dir "$package_name"
-    done <<< "$list_git_packages"
+    done <<< "$git_packages"
 }
 
 clone_and_build(){
@@ -125,55 +131,47 @@ cleanup_package_dir(){
     rm -rf "$package_name"
 }
 
-# ---------------------------------
+# vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 
-copy_packages_configurations() {
-    echo -e "\n${WHITE} [${BLUE}i${WHITE}] Copying packages configurations.\n"
-    
+copy_all_package_configurations() {
     local packages=(bspwm sxhkd kitty picom neofetch ranger cava polybar)
+
+    echo -e "${WHITE} [${BLUE}i${WHITE}] Copying packages configurations.\n"
     
     for package in "${packages[@]}"; do
-        copy_package_settings "$package"
+        copy_and_configure_package "$package"
     done
 }
 
-copy_package_settings() {
+copy_and_configure_package() {
     local package="$1"
+    local need_permissions=(bspwm sxhkd polybar)
     
-    sudo rm -rf "${HOME_DIR}/.config/$package"
-    
+    if [ -d "${HOME_DIR}/.config/$package" ]; then
+        rm -rf "${HOME_DIR}/.config/$package"
+    fi
+
     cp -r "${CURRENT_DIR}/.config/$package" "${HOME_DIR}/.config/$package"
     
-    if $package == "bspwm" || $package == "sxhkd" || $package == "polybar"; then
-        local base_folder="${HOME_DIR}/.config/$package"
-        
-        grant_permission_exe "$base_folder"
+    if [[ "${need_permissions[@]}" =~ "$package" ]]; then
+        set_executable_permissions "${HOME_DIR}/.config/$package" # Base folder
     fi
 }
 
-grant_permission_execute(){
+set_executable_permissions() {
     local base_folder="$1"
     
-    for patch_file in "$base_folder"/**/*.*; do
-        if is_bash_script "$patch_file"; then
+    find "$base_folder" -type f | while read -r patch_file; do
+        if head -n 1 "$patch_file" | grep -q '^#!'; then
             chmod +x "$patch_file"
         fi
     done
 }
 
-is_bash_script() {
-    local patch_file="$1"
-    local shebang=$(head -n 1 "$patch_file")
-    
-    if [[ $shebang == "#!"* ]]; then
-        return 0
-    else
-        return 1
-    fi
-}
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 copy_bspwm_scripts() {
-    echo -e "\n${WHITE} [${BLUE}i${WHITE}] Copying bspwm scripts.\n"
+    echo -e "${WHITE} [${BLUE}i${WHITE}] Copying bspwm scripts.\n"
 
     cp -r scripts "${HOME_DIR}"
     
@@ -182,7 +180,7 @@ copy_bspwm_scripts() {
 }
 
 copy_bspwm_themes() {
-    echo -e "\n${WHITE} [${BLUE}i${WHITE}] Copying bspwm themes.\n"
+    echo -e "${WHITE} [${BLUE}i${WHITE}] Copying bspwm themes.\n"
 
     cp -r .themes "${HOME_DIR}"
     
@@ -193,7 +191,7 @@ copy_bspwm_themes() {
 }    
 
 copy_fonts() {
-    echo -e "\n${WHITE} [${BLUE}i${WHITE}] Copying fonts."
+    echo -e "${WHITE} [${BLUE}i${WHITE}] Copying fonts."
 
     cp -r ".fonts" "${HOME_DIR}"
     
@@ -224,40 +222,28 @@ temporal() {
 # ----------
 
 main() {
-    HOME_DIR="/home/${USERNAME}"
-    CURRENT_DIR=$(pwd)
-    local banner_file="resources/banner.txt"
-    local install_dir="/usr/local/bin/"
+    source config.sh
 
     clear
-    define_colors
-    display_banner "$banner_file"
+    display_banner "${files[banner]}"
     
-    echo -ne "${WHITE} [${BLUE}?${WHITE}] Do you want to continue with the installation?: ([y]/n) ▶\t"
-    
-    tput setaf 1
-    read -r reply
-    tput setaf 0
-
-    if [[ $reply == 'y' ]]; then
-        local list_rpm_packages=$(get_list_rpm_packages "rpm_packages.yaml")
-        local list_git_packages=$(get_list_git_packages "git_packages.yaml")
-
-        echo -e "${WHITE} [${BLUE}i${WHITE}] Starting the installation process.\n" 
-
-        install_rpm_packages_from_list "$list_rpm_packages"
-        deploy_git_packages_from_list "$list_git_packages" "$install_dir"
-
-        #copy_packages_configurations
-        #copy_bspwm_scripts
-        #copy_bspwm_themes
-        #copy_fonts
-        #temporal
-
-        echo -e "\n${WHITE} [${GREEN}+${WHITE}] Installation completed, please reboot to apply the configuration."
-    else
-        echo -e "\n${WHITE} [${RED}!${WHITE}] Installation aborted."
+    if ! confirm_installation; then
+        echo -e "${bullets[surprise]} Installation aborted."
+        return
     fi
+
+    echo -e "${bullets[info]} Starting the installation process.\n" 
+
+    install_rpm_packages "${packages[rpm]}"
+    #deploy_git_packages "$GIT_PACKAGES" "$INSTALL_DIR"
+ 
+    #copy_all_package_configurations
+    #copy_bspwm_scripts
+    #copy_bspwm_themes
+    #copy_fonts
+    #temporal
+
+    echo -e "${bullets[check]} Installation completed, please reboot to apply the configuration."
 }
 
 main
