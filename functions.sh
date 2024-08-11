d7460ee6931c47ca4d8b87b347155512a0192d9e
José AZÓCAR <azocare@gmail.com>
2024-08-11 13:42:37 -0400

    Restructuring of project files

--- functions.sh
+++ functions.sh
@@ -54,6 +54,74 @@ deploy_clone(){
     done <<< "$pkgs_git"
 }
 
+configure_packages() {
+    local -n packages=$1
+    
+    echo -e "${bullets[info]} Configures packages installed from RPM:"
+    
+    for package in "${!packages[@]}"; do
+        apply_configs "${package}" "${packages[$package]}"
+    done
+}
+
+copy_new_fonts() {
+    local -n folders=$1
+    local source="${folders[source]}"
+
+    local order_keys=("user" "system")
+    local cmd_prefix=""
+
+    echo -e "${bullets[info]} Copying new fonts:"
+
+    if [[ ! -d "$source" ]]; then
+        echo -e "${bullets[error]} The source directory ${source} does not exist."
+        return 1
+    fi
+
+    for key in "${order_keys[@]}"; do
+        local target="${folders[$key]}"
+
+        [[ $key == "system" ]] && cmd_prefix="sudo "
+          
+        ${cmd_prefix}mkdir -p "${target}"
+        ${cmd_prefix}cp -rv "${source}"/* "${target}"
+        
+        source="${target}"
+
+        echo -e "${bullets[success]} Typefaces copied to ${target}"
+    done
+}
+
+process_bspwm_assets(){
+    local -a assets=("${@:1:$#-1}")
+    local target="${!#}"
+
+    if [ ${#assets[@]} -eq 0 ]; then
+        echo -e "${bullets[error]} Error: no assets specified."
+        return 1
+    fi
+    
+    echo -e "${bullets[info]} Processes bspwm resources:"
+
+    for asset in "${assets[@]}"; do
+        copy_assets "$asset" "$target"
+        add_exec_flag "$target$asset"
+    done
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 clone_repo() {
     local git_url="$1"
     local base_path="$2"
@@ -135,16 +203,6 @@ delete_work_folder(){
     rm -rf "$cleanup"
 }
 
-configure_packages() {
-    local -n packages=$1
-    
-    echo -e "${bullets[info]} Configures packages installed from RPM:"
-    
-    for package in "${!packages[@]}"; do
-        apply_configs "${package}" "${packages[$package]}"
-    done
-}
-
 apply_configs() {
     local package="$1"
     local execute="$2"
@@ -157,51 +215,6 @@ apply_configs() {
     copy_assets "${source}" "${target}"
     
     [[ "${execute}" -eq 1 ]] && add_exec_flag "${target}"
-}
-
-copy_new_fonts() {
-    local -n folders=$1
-    local source="${folders[source]}"
-
-    local order_keys=("user" "system")
-    local cmd_prefix=""
-
-    echo -e "${bullets[info]} Copying new fonts:"
-
-    if [[ ! -d "$source" ]]; then
-        echo -e "${bullets[error]} The source directory ${source} does not exist."
-        return 1
-    fi
-
-    for key in "${order_keys[@]}"; do
-        local target="${folders[$key]}"
-
-        [[ $key == "system" ]] && cmd_prefix="sudo "
-          
-        ${cmd_prefix}mkdir -p "${target}"
-        ${cmd_prefix}cp -rv "${source}"/* "${target}"
-        
-        source="${target}"
-
-        echo -e "${bullets[success]} Typefaces copied to ${target}"
-    done
-}
-
-process_bspwm_assets(){
-    local -a assets=("${@:1:$#-1}")
-    local target="${!#}"
-
-    if [ ${#assets[@]} -eq 0 ]; then
-        echo -e "${bullets[error]} Error: no assets specified."
-        return 1
-    fi
-    
-    echo -e "${bullets[info]} Processes bspwm resources:"
-
-    for asset in "${assets[@]}"; do
-        copy_assets "$asset" "$target"
-        add_exec_flag "$target$asset"
-    done
 }
 
 copy_assets() {
@@ -251,6 +264,16 @@ is_valid_asset() {
 
     return 0
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
