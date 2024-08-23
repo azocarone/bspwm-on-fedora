c8b5a97019a6067005c83d606633183453bca0fa
José AZÓCAR <azocare@gmail.com>
2024-08-23 06:40:58 -0400

    Restructuring of project files

---
+++ helpers/common.sh
@@ -0,0 +1,40 @@
+determine_clone_path() {
+    local url="$1"
+    local base_path="${2:-${paths[current]}}"
+    
+    local absolute_path
+    local repo_name=$(basename "${url}" .git)
+    
+    if [[ "${base_path}" == *"/." ]]; then
+        # Case 1: If base_path ends in “/.”, do not add “/”.
+        absolute_path="${base_path}${repo_name}"
+    else
+        # Case 2: In any other case, add “/”.
+        absolute_path="${base_path}/${repo_name}"
+    fi
+
+    echo "${absolute_path}"
+}
+
+has_install_script() {
+    local absolute_path="$1"
+    local pattern="install*"
+
+    if find "$absolute_path" -maxdepth 1 -name "$pattern" -type f | grep -q .; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+file_or_directory_exists() {
+    local asset="$1"
+
+    if [[ ! -e "$asset" ]]; then
+        echo -e "${bullets[error]} Error: ${colors[red]}${asset}${colors[white]} is not a file or a directory."
+        return 1
+    fi
+
+    return 0
+}
